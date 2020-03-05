b23d0c6292cb8eabe826b82f07bf02a0bd4782dc
Danish Rafique <danishrafique0154@gmail.com>
2020-03-05 08:33:51 +0530

    Profile Integration #1

--- screens/ProfileScreen/component/profileInfo.js
+++ screens/ProfileScreen/component/profileInfo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "../styles/profileInfoStyle";
 import { View, Image } from "react-native";
 import { Text } from "@ui-kitten/components";
--- screens/ProfileScreen/index.js
+++ screens/ProfileScreen/index.js
@@ -1,20 +1,32 @@
 import React from "react";
-import { StyleSheet } from "react-native";
-import { Layout, Text, Toggle } from "@ui-kitten/components";
+import { ScrollView } from "react-native";
+import { Layout, Toggle, Input } from "@ui-kitten/components";
 import TopBar from "../../components/TopBar";
 import { ThemeContext } from "../../contexts/theme-context";
 import ProfileInfo from "./component/profileInfo";
+import styles from "./profileStyle";
+
+
+const useInputChanges = (initialValue = '') => {
+  const [value, setValue] = React.useState(initialValue);
+  return {
+    value,
+    onChangeText: setValue,
+  };
+};
 
 const ProfileScreen = () => {
   const [checked, setChecked] = React.useState(false);
   const themeContext = React.useContext(ThemeContext);
+  const largeInputChanges = useInputChanges();
+
   const profileData = {
     name: "Danish Rafique",
     tag: "Budding Chef",
     designation: "Su Chef @Mojopizza",
-    storyCount : 40,
-    followerCount : 200,
-    couponCount : 5,
+    storyCount: 40,
+    followerCount: 200,
+    couponCount: 5,
     imageSource: require("../../assets/avatar.jpg")
   }
 
@@ -25,20 +37,24 @@ const ProfileScreen = () => {
   return (
     <Layout style={styles.container}>
       <TopBar />
-      <ProfileInfo data={profileData} />
-      <Toggle
-        text={`Light/Dark Mode`}
-        checked={checked}
-        onChange={onCheckedChange}
-      />
+      <ScrollView style={styles.container}>
+        <ProfileInfo data={profileData} />
+        <Input
+          style={styles.input}
+          size='large'
+          placeholder="What's Cooking Today? add a post to your Dailykit's journey.."
+          multiline={true}
+          numberOfLines={4}
+          {...largeInputChanges}
+        />
+        <Toggle
+          text={`Light/Dark Mode`}
+          checked={checked}
+          onChange={onCheckedChange}
+        />
+      </ScrollView>
     </Layout>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  }
-});
 
 export default ProfileScreen;
